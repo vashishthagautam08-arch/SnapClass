7267a29a71becdeef39cad9784707e45b9d65d85
Gautam Vashishtha <gautamvashishtha@Gautams-MacBook-Air.local>
2026-07-03 21:45:36 +0530

    Update SnapClass

--- src/components/dialog_voice_attendance.py
+++ src/components/dialog_voice_attendance.py
@@ -1,32 +1,70 @@
 import streamlit as st
 
-import segno
-import io
+from src.pipelines.voice_pipeline import process_bulk_audio
+
+from src.database.config import supabase
+
+import pandas as pd
 
 
-@st.dialog("Share Class Link")
-def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-main.streamlit.app"
-    join_url = f"{app_domain}/?join-code={subject_code}"
+from src.components.dialog_attendance_results import show_attendance_result
+from datetime import datetime
+@st.dialog('Voice Attendance')
+def voice_attendance_dialog(selected_subject_id):
+    st.write('Record audio of students saying I am present. Then AI will recognize the students')
 
-    st.header("Scan to Join")
 
-    qr = segno.make(join_url)
+    audio_data = None
 
-    out = io.BytesIO()
+    audio_data = st.audio_input("Record classroom audio")
 
-    qr.save(out, kind='png', scale=10, border=1)
+    if st.button('Analyze Audio', width='stretch', type='primary'):
+        with st.spinner('Prcessing Audio data'):
+            enrolled_res = supabase.table('subject_students').select("*, students(*)").eq('subject_id',selected_subject_id ).execute()
+            enrolled_students = enrolled_res.data
 
-    col1, col2 = st.columns(2)
+            if not enrolled_students:
+                st.warning('No students enrolled in this course')
+                return
+            candidates_dict = {
+                s['students']['student_id'] : s['students']['voice_embedding'] 
+                for s in enrolled_students if s['students'].get('voice_embedding')
+            }
 
-    with col1:
-        st.markdown('### Copy Link')
-        st.code(join_url, language="text")
-        st.code(subject_code, language="text")
-        st.info('Copy this link to share on Whatsapp or Email')
+            if not candidates_dict:
+                st.error('No enrolled students have voice profiles registerd')
+                return
+            
+            audio_bytes = audio_data.read()
 
-    with col2:
-        st.markdown('### Scan to Join')
-        st.image(out.getvalue(), caption='QRCODE for class joining')
+            detected_scores = process_bulk_audio(audio_bytes, candidates_dict)
 
-        
+            results, attendance_to_log  = [], []
+
+            current_timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+
+            for node in enrolled_students:
+                student = node['students']
+                score  = detected_scores.get(student['student_id'], 0.0)
+                is_present= bool(score>0)
+
+                results.append({
+                    "Name": student['name'],
+                    "ID": student['student_id'],
+                    "Source": score if is_present else "-",
+                    "Status": "✅ Present" if is_present else "❌ Absent"
+                })
+
+                attendance_to_log.append({
+                    'student_id': student['student_id'],
+                    'subject_id': selected_subject_id,
+                    'timestamp': current_timestamp,
+                    'is_present': bool(is_present)
+                })
+            st.session_state.voice_attendance_results = (pd.DataFrame(results), attendance_to_log)
+
+    if st.session_state.get('voice_attendance_results'):
+        st.divider()
+        df_results, logs = st.session_state.voice_attendance_results
+        show_attendance_result(df_results, logs)
